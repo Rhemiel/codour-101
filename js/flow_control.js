1e9d6939de39471e674becb6783f2e43ec192a07
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-11-09 07:48:04 +0800

    Massive Updates

--- js/flow_control.js
+++ js/flow_control.js
@@ -1,3 +1,37 @@
+//Startup
+function setRandomValueWithExpiration() {
+    // Generate a random value
+    const randomValue = Math.random().toString(36).substr(2, 8);
+  
+    // Set the random value in localStorage
+    localStorage.setItem('randomValue', randomValue);
+  
+    // Set an expiration timestamp 12 hours from now
+    const expirationTime = new Date().getTime() + 12 * 60 * 60 * 1000;
+    localStorage.setItem('expirationTime', expirationTime);
+  }
+  
+  function getRandomValue() {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (!expirationTime || new Date().getTime() > expirationTime) {
+      // The data has expired or doesn't exist; remove it
+      localStorage.removeItem('randomValue');
+      localStorage.removeItem('expirationTime');
+      return null;
+    }
+  
+    // Data is still valid, return the random value
+    return localStorage.getItem('randomValue');
+  }
+  
+  // Set a random value with a 12-hour expiration
+  setRandomValueWithExpiration();
+  
+  // Get the random value
+  const randomValue = getRandomValue();
+  console.log(randomValue);
+  
+  
 
 //Control Variables
 const sbNav = document.getElementById("sb");
@@ -49,6 +83,80 @@ function showSelectedOption(event){
         }
     }
 }
+
+//Check Answers
+function getInputValuesByDataGroup(dataGroup) {
+    const elements = document.querySelectorAll(`[data-group="${dataGroup}"]`);
+    const values = [];
+  
+    elements.forEach(element => {
+      if (element.tagName === 'INPUT' && element.type === 'text') {
+        values.push(element.value);
+      }
+    });
+  
+    return values;
+}
+
+function getDataAnswersByGroup(dataGroup) {
+    const elements = document.querySelectorAll(`[data-group="${dataGroup}"]`);
+    const dataAnswers = [];
+  
+    elements.forEach(element => {
+      const dataAnswer = element.getAttribute('data-answer');
+      dataAnswers.push(dataAnswer);
+    });
+  
+    return dataAnswers;
+}
+
+function arraysAreEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+  
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  
+    return true;
+}
+
+function correctAns(dataGroup) {
+    const elements = document.querySelectorAll(`[data-group="${dataGroup}"]`);
+  
+    elements.forEach(element => {
+      element.classList.add('correct');
+    });
+}
+
+function wrongAns(dataGroup) {
+    const elements = document.querySelectorAll(`[data-group="${dataGroup}"]`);
+  
+    elements.forEach(element => {
+      element.classList.add('wrong');
+    });
+}
+
+
+function checkAns(id){
+    const scoreboard = document.getElementById("scoreboard");
+    var score = 0;
+    const DataAnswers = getDataAnswersByGroup(id);
+    const playerAnswer = getInputValuesByDataGroup(id);
+    if(arraysAreEqual(DataAnswers, playerAnswer)){
+        score += 1;
+        scoreboard.innerText = score;
+        correctAns(id);
+    }else{
+        wrongAns(id);
+    }
+}
+
+
+
 
 
 sbButton.addEventListener("click", showNav);
